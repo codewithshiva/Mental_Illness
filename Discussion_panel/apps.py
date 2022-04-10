b01d8be5f4872ebf8a03ebf6a5f32eba754e64e1
Shivam Singh <51095912+codewithshiva@users.noreply.github.com>
2022-04-10 10:52:14 +0530

    y

---
+++ Discussion_panel/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DiscussionPanelConfig(AppConfig):
+    name = 'Discussion_panel'
---
+++ Mental_Illness/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Mental_Illness project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Mental_Illness.settings')
+
+application = get_asgi_application()
